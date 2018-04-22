61027ab80e40712142cc86bfea8c6028aaa05628
Jason <jason.renard@gmail.com>
2018-04-21 22:40:32 -0400

    Temperature graphs work.

--- cgi/temperature_history.py
+++ cgi/temperature_history.py
@@ -4,13 +4,46 @@
 import os
 import cgitb
 import sqlite3
+import datetime
 cgitb.enable()
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+def generate_png( cursor, start, end, filename ):
+    query = '''SELECT * FROM "temps" WHERE read_datetime BETWEEN "%s" AND 
+    "%s"''' % ( start, end )
+
+    cursor.execute( query )
+
+    temp_list = []
+    print( '<p>' )
+    all = cursor.fetchall()
+    for thing in all:
+        #print(thing, '<br>')
+        temp_list += thing[1].strip('[]').split(',')
+
+    float_temps = [float(temp) for temp in temp_list]
+
+    print( '</p>' )
+
+    plt.plot(float_temps)
+    plt.savefig('graphs/%s' % (filename))
+
 
 # Print necessary headers.
 print("Content-Type: text/html" )
 print('')
 
 print("temperature history")
+
+today=datetime.datetime.now()
+today_str = today.strftime("%Y-%m-%dT%H:%M:%S")
+print("today: ", today_str )
+lasttwentyfour = today - datetime.timedelta( seconds = 24*60*60 )
+lasttwentyfour_str = lasttwentyfour.strftime("%Y-%m-%dT%H:%M:%S")
+print("lasttwentyfour: ", lasttwentyfour_str )
 
 try:
     db = sqlite3.connect('./env_stats.db' )
@@ -22,14 +55,20 @@ try:
 except:
     print( 'db.cursor() failed' )
 
-query = '''SELECT * FROM "temps" WHERE read_datetime BETWEEN "2018-03-22" AND 
-"2018-03-31"'''
+generate_png( cursor, lasttwentyfour_str, today_str, '24hours.png' )
 
-cursor.execute( query )
+twoweeks = today - datetime.timedelta( days=14)
+twoweeks_str = twoweeks.strftime("%Y-%m-%dT%H:%M:%S")
+print( "<br>twoweeks_str: ", twoweeks_str )
+generate_png( cursor, twoweeks_str, today_str, '2weeks.png' )
 
-all = cursor.fetchall()
-for thing in all:
-    print(thing, '<br>')
+print( '<h2>Last 24 Hours</h2>' )
+print( '<img src="graphs/24hours.png">' )
+
+print( '<h2>Last 2 Weeks</h2>' )
+print( '<img src="graphs/2weeks.png">' )
+
+print( '<h2>Last 24 Hours</h2>' )
+print( '<img src="graphs/lastyear.png">' )
 
 db.close()
-
